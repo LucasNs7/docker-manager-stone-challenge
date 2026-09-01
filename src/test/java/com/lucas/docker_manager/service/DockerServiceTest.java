b0bff9ef66b384b1ca5f5b76df146f64efcc2f3e
Lucas Sousa <lucas.sousadev07@gmail.com>
2026-09-01 16:17:23 -0300

    feat: add more tests for ServiceTest

--- src/test/java/com/lucas/docker_manager/service/DockerServiceTest.java
+++ src/test/java/com/lucas/docker_manager/service/DockerServiceTest.java
@@ -1,10 +1,10 @@
 package com.lucas.docker_manager.service;
 
 import com.github.dockerjava.api.DockerClient;
-import com.github.dockerjava.api.command.ListContainersCmd;
-import com.github.dockerjava.api.command.StartContainerCmd;
+import com.github.dockerjava.api.command.*;
 import com.github.dockerjava.api.exception.NotFoundException;
 import com.github.dockerjava.api.model.Container;
+import com.github.dockerjava.api.model.Image;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -21,6 +21,18 @@ class DockerServiceTest {
 
     @Mock
     private DockerClient dockerClient;
+
+    @Mock
+    private StopContainerCmd stopContainerCmd;
+
+    @Mock
+    private RemoveContainerCmd removeContainerCmd;
+
+    @Mock
+    private CreateContainerCmd createContainerCmd;
+
+    @Mock
+    private ListImagesCmd listImagesCmd;
 
     @Mock
     private ListContainersCmd listContainersCmd;
@@ -99,5 +111,120 @@ class DockerServiceTest {
 
         verify(dockerClient).startContainerCmd(containerId);
         verify(startContainerCmd).exec();
+    }
+
+    @Test
+    @DisplayName("Deve consultar todas as imagens disponíveis")
+    void testListImages() {
+        // Mock test
+        List<Image> mockImagesList = Collections.emptyList();
+
+        when(dockerClient.listImagesCmd()).thenReturn(listImagesCmd);
+        when(listImagesCmd.exec()).thenReturn(mockImagesList);
+
+        // Normal Test
+        List<Image> images = dockerService.listImages();
+
+        // Verificações
+        assertEquals(mockImagesList, images);
+        verify(dockerClient).listImagesCmd();
+        verify(listImagesCmd).exec();
+    }
+
+    @Test
+    @DisplayName("Deve filtrar imagens pelo nome com sucesso")
+    void testFilteredImages() {
+        // Mock test
+        String imageName = "ubuntu:latest";
+        List<Image> mockImagesList = Collections.emptyList();
+
+        when(dockerClient.listImagesCmd()).thenReturn(listImagesCmd);
+        when(listImagesCmd.withFilter("reference", Collections.singletonList(imageName))).thenReturn(listImagesCmd);
+        when(listImagesCmd.exec()).thenReturn(mockImagesList);
+
+        // Normal Test
+        List<Image> images = dockerService.filteredImages(imageName);
+
+        // Verificações
+        assertEquals(mockImagesList, images);
+        verify(dockerClient).listImagesCmd();
+        verify(listImagesCmd).withFilter("reference", Collections.singletonList(imageName));
+        verify(listImagesCmd).exec();
+    }
+
+    @Test
+    @DisplayName("Deve testar se o id recebido está sendo passado para parar o container")
+    void testStopContainer() {
+        String containerId = UUID.randomUUID().toString();
+        when(dockerClient.stopContainerCmd(eq(containerId))).thenReturn(stopContainerCmd);
+
+        dockerService.stopContainer(containerId);
+
+        verify(dockerClient).stopContainerCmd(containerId);
+        verify(stopContainerCmd).exec();
+    }
+
+    @Test
+    @DisplayName("Deve testar se o stopContainer está lançando a exceção NotFoundException ao tentar parar container inexistente")
+    void testStopContainerNotFound() {
+        String containerId = UUID.randomUUID().toString();
+        when(dockerClient.stopContainerCmd(eq(containerId))).thenReturn(stopContainerCmd);
+        when(stopContainerCmd.exec()).thenThrow(new NotFoundException("Container Not Found"));
+
+        assertThrows(NotFoundException.class, () -> dockerService.stopContainer(containerId));
+
+        verify(dockerClient).stopContainerCmd(containerId);
+        verify(stopContainerCmd).exec();
+    }
+
+    @Test
+    @DisplayName("Deve testar se o id recebido está sendo passado para deletar o container")
+    void testDeleteContainer() {
+        String containerId = UUID.randomUUID().toString();
+        when(dockerClient.removeContainerCmd(eq(containerId))).thenReturn(removeContainerCmd);
+
+        dockerService.deleteContainer(containerId);
+
+        verify(dockerClient).removeContainerCmd(containerId);
+        verify(removeContainerCmd).exec();
+    }
+
+    @Test
+    @DisplayName("Deve testar se o deleteContainer está lançando a exceção NotFoundException ao tentar deletar container inexistente")
+    void testDeleteContainerNotFound() {
+        String containerId = UUID.randomUUID().toString();
+        when(dockerClient.removeContainerCmd(eq(containerId))).thenReturn(removeContainerCmd);
+        when(removeContainerCmd.exec()).thenThrow(new NotFoundException("Container Not Found"));
+
+        assertThrows(NotFoundException.class, () -> dockerService.deleteContainer(containerId));
+
+        verify(dockerClient).removeContainerCmd(containerId);
+        verify(removeContainerCmd).exec();
+    }
+
+    @Test
+    @DisplayName("Deve testar a criação de container a partir do nome da imagem")
+    void testCreateContainer() {
+        String imageName = "nginx:latest";
+        when(dockerClient.createContainerCmd(eq(imageName))).thenReturn(createContainerCmd);
+        when(createContainerCmd.exec()).thenReturn(null);
+
+        dockerService.createContainer(imageName);
+
+        verify(dockerClient).createContainerCmd(imageName);
+        verify(createContainerCmd).exec();
+    }
+
+    @Test
+    @DisplayName("Deve testar se o createContainer lança exceção ao tentar criar container com imagem inexistente")
+    void testCreateContainerNotFound() {
+        String imageName = "invalid-image:latest";
+        when(dockerClient.createContainerCmd(eq(imageName))).thenReturn(createContainerCmd);
+        when(createContainerCmd.exec()).thenThrow(new NotFoundException("Image Not Found"));
+
+        assertThrows(NotFoundException.class, () -> dockerService.createContainer(imageName));
+
+        verify(dockerClient).createContainerCmd(imageName);
+        verify(createContainerCmd).exec();
     }
 }
